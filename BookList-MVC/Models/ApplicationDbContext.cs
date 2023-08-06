c9e4bd7d77b6cca7027488304b0a133757e872cb
NKChang <khacchang128@gmail.com>
2023-08-06 20:42:50 +0700

    first

---
+++ BookList-MVC/Models/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BookList_MVC.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ BookList-MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookList_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
